a42e0fb93c363a6d492d9f79f4c59c0a3964630d
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2018-04-08 14:59:26 +0800

    Remove async function

--- dist/util.js
+++ dist/util.js
@@ -3,49 +3,8 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-
-var canvasToBlob = exports.canvasToBlob = function () {
-  var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee(canvas, type) {
-    var result, dataURL, buffer;
-    return regeneratorRuntime.wrap(function _callee$(_context) {
-      while (1) {
-        switch (_context.prev = _context.next) {
-          case 0:
-            if (!canvas.toBlob) {
-              _context.next = 5;
-              break;
-            }
-
-            result = new Promise(function (resolve) {
-              canvas.toBlob(function (blob) {
-                return resolve(blob);
-              }, type);
-            });
-            return _context.abrupt("return", result);
-
-          case 5:
-            dataURL = canvas.toDataURL(type);
-            buffer = dataUrlToArrayBuffer(dataURL);
-            return _context.abrupt("return", new Blob([buffer], { type: type }));
-
-          case 8:
-          case "end":
-            return _context.stop();
-        }
-      }
-    }, _callee, this);
-  }));
-
-  return function canvasToBlob(_x, _x2) {
-    return _ref.apply(this, arguments);
-  };
-}();
-
 exports.dataUrlToArrayBuffer = dataUrlToArrayBuffer;
 exports.fileToUrl = fileToUrl;
-
-function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
-
 function dataUrlToArrayBuffer(dataURI) {
   var byteString = atob(dataURI.split(",")[1]);
   var ia = new Uint8Array(byteString.length);
@@ -64,3 +23,20 @@ function fileToUrl(file) {
     return "";
   }
 }
+
+var canvasToBlob = exports.canvasToBlob = function canvasToBlob(canvas, type) {
+  return new Promise(function (resolve) {
+    if (canvas.toBlob) {
+      var result = new Promise(function (resolve) {
+        canvas.toBlob(function (blob) {
+          return resolve(blob);
+        }, type);
+      });
+      resolve(result);
+    } else {
+      var dataURL = canvas.toDataURL(type);
+      var buffer = dataUrlToArrayBuffer(dataURL);
+      resolve(new Blob([buffer], { type: type }));
+    }
+  });
+};
--- src/util.js
+++ src/util.js
@@ -17,15 +17,16 @@ export function fileToUrl(file) {
   }
 }
 
-export async function canvasToBlob(canvas, type) {
-  if (canvas.toBlob) {
-    const result = new Promise(resolve => {
-      canvas.toBlob(blob => resolve(blob), type);
-    });
-    return result;
-  } else {
-    const dataURL = canvas.toDataURL(type);
-    const buffer = dataUrlToArrayBuffer(dataURL);
-    return new Blob([buffer], { type });
-  }
-}
+export const canvasToBlob = (canvas, type) =>
+  new Promise(resolve => {
+    if (canvas.toBlob) {
+      const result = new Promise(resolve => {
+        canvas.toBlob(blob => resolve(blob), type);
+      });
+      resolve(result);
+    } else {
+      const dataURL = canvas.toDataURL(type);
+      const buffer = dataUrlToArrayBuffer(dataURL);
+      resolve(new Blob([buffer], { type }));
+    }
+  });
