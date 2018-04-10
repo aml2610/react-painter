691568d9f39190d602608a0f17bb7e4ae08a7f98
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2018-04-11 04:04:27 +0800

    added tests

---
+++ test/typescript.tsx
@@ -0,0 +1,28 @@
+const library = require('react-testing-library');
+import * as React from 'react';
+import { ReactPainter } from '../src';
+
+afterEach(() => {
+  library.cleanup();
+});
+
+test('render empty and save', async () => {
+  const saveHandler = jest.fn();
+  const { getByText, container } = library.renderIntoDocument(
+    <ReactPainter
+      onSave={saveHandler}
+      render={({ canvas, triggerSave }) => (
+        <div>
+          {canvas}
+          <button onClick={triggerSave}>Save Canvas</button>
+        </div>
+      )}
+    />
+  );
+
+  library.Simulate.click(getByText('Save Canvas'));
+  await library.wait();
+  expect(saveHandler).toBeCalled();
+  expect(saveHandler.mock.calls.length).toBe(1);
+  expect(saveHandler.mock.calls[0][0]).toBeInstanceOf(Blob);
+});
