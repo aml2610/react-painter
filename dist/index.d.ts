e49c2adca6bc0d04130c3dfa696c26fdb077dda3
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2018-04-15 07:12:28 +0800

    add travis npm publish and remove dist folder

--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { ReactPainter } from './ReactPainter';
-export { ReactPainter } from './ReactPainter';
-export default ReactPainter;
--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var ReactPainter_1 = require("./ReactPainter");
-var ReactPainter_2 = require("./ReactPainter");
-exports.ReactPainter = ReactPainter_2.ReactPainter;
-exports.default = ReactPainter_1.ReactPainter;
-//# sourceMappingURL=index.js.map
